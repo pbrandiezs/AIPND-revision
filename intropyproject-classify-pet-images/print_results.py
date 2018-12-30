34862d25b60b4a1a7279a5b7978eeed3258369ac
Perry Brandiezs <pbrandiezs@gmail.com>
2018-12-30 09:08:08 -0800

    print_results.py updated header docs

--- intropyproject-classify-pet-images/print_results.py
+++ intropyproject-classify-pet-images/print_results.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:
+# PROGRAMMER: Perry Brandiezs
+# DATE CREATED: December 30, 2018
 # REVISED DATE: 
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
